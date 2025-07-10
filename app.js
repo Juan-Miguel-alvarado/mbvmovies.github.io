aa4c70d3b3652156f848473466537e7178618ef7
Miguel :V <juanchomigue2012@gmail.com>
2025-07-10 09:56:23 -0500

    MBV - Movies (Solving bugs - add more information to movies)

--- app.js
+++ app.js
@@ -44,26 +44,66 @@ async function fetchMovies() {
     const data = await response.json();
     const movies = data.results;
 
+    let poster = "https://image.tmdb.org/t/p/w500"
+    let NoPoster = "https://lh3.googleusercontent.com/proxy/wjRXrOcYxJ4gNWcJNYegLKHOdQfiNqPX5U1Xj8ud1WWh1WkZTuVIfd7HKeencjABBa4";
+
     if (movies.length === 0) {
-      moviesContainer.innerHTML = "<p>We dirent find movies ¡Sorry!.</p>";
+      setTimeout(() => {
+        loader.style.display = "none";
+        moviesContainer.innerHTML = "<h3>We dirent find movies ¡Sorry!.</h3>";
+        return;
+      }, 2000); 
     } else {
       setTimeout(() => {
         movies.forEach((movie) => {
+          const MoreInformation = document.createElement("div");
           const div = document.createElement("div");
           div.className = "movie";
           div.innerHTML = `
             <h2>${movie.title} (${movie.release_date ? movie.release_date.slice(0, 4) : "N/A"})</h2> 
-            <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="">
-            <p>${movie.overview ? movie.overview.slice(0, 150) + "..." : "No desc."}</p>
+            <img src="${movie.poster_path ? poster + movie.poster_path : NoPoster}" alt="">
+            <p>${movie.overview ? movie.overview.slice(0, 150) + "..." : "No description..."}</p>
           `;
           moviesContainer.appendChild(div);
+
+          div.addEventListener("click", () => {
+            MoreInformation.className = "more-information";
+            MoreInformation.innerHTML = `
+              <div>
+                <img src="${movie.poster_path ? poster + movie.poster_path : NoPoster}" alt="">
+              </div>
+              <div class="info">
+                <h2>${movie.title} (${movie.release_date ? movie.release_date.slice(0, 4) : "N/A"})</h2>
+                <p class="desc">${movie.overview ? movie.overview : "No description..."}</p>
+                <p><strong>Rating:</strong> ${movie.vote_average ? movie.vote_average : "N/A"}</p>
+                <p><strong>Release Date:</strong> ${movie.release_date ? movie.release_date : "N/A"}</p>
+                <button class="close-btn" id="xBtn">X</button>
+              </div>
+            `;
+            moviesContainer.appendChild(MoreInformation);
+            setTimeout(() => {
+              MoreInformation.style.opacity = "1";
+            }, 100); 
+
+            const xBtn = document.getElementById("xBtn");
+            xBtn.addEventListener("click", () => {
+              MoreInformation.style.opacity = "0";
+              setTimeout(() => {
+                MoreInformation.remove();
+              }, 1000);
+            });
+          });
+
         });
       }, 2000); 
     }
 
   } catch (error) {
-    console.error("Something is bad:", error);
-    moviesContainer.innerHTML = "<p>Something gone worng.</p>";
+    setTimeout(() => {
+      loader.style.display = "none";
+      console.error("Something is bad:", error);
+      moviesContainer.innerHTML = "<h3>Something gone worng.</h3>";
+    }, 2000); 
   } finally {
     setTimeout(() => {
       loader.style.display = "none";
